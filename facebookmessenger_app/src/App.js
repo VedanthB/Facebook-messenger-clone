21b5d59db6ab6269e0c0bc298451523f38839ac2
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-02-14 19:35:25 +0530

    created input field

--- facebookmessenger_app/src/App.js
+++ facebookmessenger_app/src/App.js
@@ -1,9 +1,17 @@
+import React , { useState }from 'react';
 import './App.css';
 
 function App() {
+     const [input, setInput] = useState('');
+
+    console.log('🧜🏼‍♀️', input)
+
   return (
     <div className="App">
       <h1>hello world</h1>
+
+      <input value={input} onChange = {event => setInput(event.target.value)} />
+      <button>Send message</button>
  
     </div>
   );
