78553ceca1fc8d716b05bc46181726f6aa8490a0
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-02-15 18:06:02 +0530

    added ability to send message

--- facebookmessenger_app/src/App.js
+++ facebookmessenger_app/src/App.js
@@ -3,15 +3,35 @@ import './App.css';
 
 function App() {
      const [input, setInput] = useState('');
+     const [messages, setMessages] = useState(['hello','hi']);
+   
+     console.log('🧜🏼‍♀️', input);
+     console.log('🧜🏼‍♀️', messages);
 
-    console.log('🧜🏼‍♀️', input)
+    const sendMessage = (event) => {
+        // this fires when the button is clicked 
+        event.preventDefault();  // prevents Reload/Refresh
+        setMessages([...messages, input]);
+        setInput('') // clears the input after submit
+
+    }
 
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>hello world</h1> 
 
-      <input value={input} onChange = {event => setInput(event.target.value)} />
-      <button>Send message</button>
+      <form>
+      <input value={input}   onChange = {event => setInput(event.target.value)} />
+      <button type='submit' onClick={sendMessage} >Send message</button>
+      </form>
+
+
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+
+      }
  
     </div>
   );
